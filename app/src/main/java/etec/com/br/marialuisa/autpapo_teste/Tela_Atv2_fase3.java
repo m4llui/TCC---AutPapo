1009dba806657e7910527bb37ab7b13380815cad
m4llui <malu.snasc24@gmail.com>
2024-08-24 15:21:54 -0300

    Update Tela_Atv2_fase3.java

--- app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Atv2_fase3.java
+++ app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Atv2_fase3.java
@@ -10,5 +10,6 @@ public class Tela_Atv2_fase3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_atv2_fase3);
+        //foi omggg
     }
 }
