f8595b5b138992572228d80a4690ba71ed1cdd54
m4llui <malu.snasc24@gmail.com>
2024-08-24 15:49:54 -0300

    Projeto AutPapo

---
+++ app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Atv4_fase3.java
@@ -0,0 +1,14 @@
+package etec.com.br.marialuisa.autpapo_teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Atv4_fase3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_atv4_fase3);
+    }
+}
