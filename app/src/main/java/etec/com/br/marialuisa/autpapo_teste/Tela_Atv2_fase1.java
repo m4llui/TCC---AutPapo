84d5d158b52dcfbe6db41f17779faf8d40b83ac8
LimaSt <gustavo.stollone02@gmail.com>
2024-08-25 13:24:57 -0300

    Projeto AutPapo

---
+++ app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Atv2_fase1.java
@@ -0,0 +1,96 @@
+package etec.com.br.marialuisa.autpapo_teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Tela_Atv2_fase1 extends AppCompatActivity {
+
+    private ImageView btn_Let_A, btn_Let_A_Inc, btn_Let_B, btn_Let_B_Inc, btn_Let_E, btn_Let_E_Certo,
+            btn_Let_Y, getBtn_Let_Y_Inc, btVoltar3, btEnunciado, btBalao;
+    private Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_atv2_fase1);
+
+
+        btn_Let_A = findViewById(R.id.btn_Let_A_2);
+        btn_Let_A_Inc = findViewById(R.id.btn_Let_A_Inc);
+        btn_Let_B = findViewById(R.id.btn_Let_B_Inc2);
+        btn_Let_B_Inc = findViewById(R.id.btn_Let_B_Inc2);
+        btn_Let_E = findViewById(R.id.btn_Let_E_2);
+        btn_Let_E_Certo = findViewById(R.id.btn_Let_E_Certo);
+        btn_Let_Y = findViewById(R.id.btn_Let_Y);
+        getBtn_Let_Y_Inc = findViewById(R.id.btn_Let_Y_Inc);
+        btVoltar3 = findViewById(R.id.btnVoltar3);
+        btEnunciado = findViewById(R.id.ImageEnunciado2);
+        btBalao = findViewById(R.id.ImageBalao2);
+
+
+        botoesInativados();
+
+
+        setOnClickListeners();
+    }
+
+    private void botoesInativados() {
+
+        btn_Let_A_Inc.setVisibility(View.INVISIBLE);
+        btn_Let_B_Inc.setVisibility(View.INVISIBLE);
+        btn_Let_E_Certo.setVisibility(View.INVISIBLE);
+        getBtn_Let_Y_Inc.setVisibility(View.INVISIBLE);
+
+
+        btn_Let_A_Inc.setEnabled(false);
+        btn_Let_B_Inc.setEnabled(false);
+        btn_Let_E_Certo.setEnabled(false);
+        getBtn_Let_Y_Inc.setEnabled(false);
+    }
+
+    private void setOnClickListeners() {
+        btn_Let_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_Let_E_Certo.setVisibility(View.VISIBLE);
+                btn_Let_E_Certo.setEnabled(true);
+
+            }
+        });
+
+        btn_Let_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_Let_A_Inc.setVisibility(View.VISIBLE);
+                btn_Let_A_Inc.setEnabled(true);
+
+            }
+        });
+
+        btn_Let_B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_Let_B_Inc.setVisibility(View.VISIBLE);
+                btn_Let_B_Inc.setEnabled(true);
+
+            }
+        });
+
+        btn_Let_Y.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getBtn_Let_Y_Inc.setVisibility(View.VISIBLE);
+                getBtn_Let_Y_Inc.setEnabled(true);
+
+            }
+        });
+    }
+}
+
+
