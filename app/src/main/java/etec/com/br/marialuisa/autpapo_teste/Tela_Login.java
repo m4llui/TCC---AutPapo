6ee89bac88ff8ef52ffb8347be94742dedc7217d
m4llui <malu.snasc24@gmail.com>
2024-09-23 16:31:39 -0300

    Projeto AutPapo - Atualizado 23/09

--- app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Login.java
+++ app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Login.java
@@ -3,7 +3,9 @@ package etec.com.br.marialuisa.autpapo_teste;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -16,6 +18,7 @@ public class Tela_Login extends AppCompatActivity {
     EditText edEmail, edSenha;
     Button btLogar;
     TextView txSaibaMais;
+    String tela;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -29,13 +32,47 @@ public class Tela_Login extends AppCompatActivity {
         btLogar = findViewById(R.id.btnLogarL);
         txSaibaMais = findViewById(R.id.SaibaMaisColetaDadosL);
 
-        //CHECKBOX FUNCIONANDO
-        if (checkBox.isChecked()) {
-            checkBox.setChecked(false);
-            //pode ir para a home
-        }else {
-            Toast.makeText(this, "Selecione a checkbox do 'Estou ciente' para a coleta de dados!", Toast.LENGTH_SHORT).show();
+        txSaibaMais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent abrirPriv = new Intent(Tela_Login.this, Tela_Politica_Priv.class);
+                abrirPriv.putExtra("login",tela);
+                startActivity(abrirPriv);
+            }
+        });
+
+        btLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edEmail.getText().toString();
+                String senha = edSenha.getText().toString();
+
+                if (email.isEmpty()) {
+                    edEmail.setError("Email obrigatório!");
+                    edEmail.requestFocus();
+                }
+                else if (!email.contains("@") || !email.endsWith(".com")) {
+                    edEmail.setError("Email inválido! Certifique-se que contém '@' e termina com '.com'.");
+                    edEmail.requestFocus();
+                }
+                else if (senha.isEmpty()) {
+                    edSenha.setError("Senha obrigatória!");
+                    edSenha.requestFocus();
+                }
+                else if (senha.length() < 4 || senha.length() > 8) {
+                    edSenha.setError("A senha deve ter entre 4 e 8 caracteres!");
+                    edSenha.requestFocus();
+                }
+                else if (!checkBox.isChecked()) {
+                    Toast.makeText(Tela_Login.this, "Selecione a checkbox do 'Estou ciente' para a coleta de dados!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //BANCO E IR PARA HOME
+                    Toast.makeText(Tela_Login.this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show();
+                    // Lógica de login aqui, como abrir uma nova Activity
+                }
+            }
+        });
     }
 }
-
-    }
--- app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Politica_Priv.java
+++ app/src/main/java/etec/com/br/marialuisa/autpapo_teste/Tela_Politica_Priv.java
@@ -2,13 +2,43 @@ package etec.com.br.marialuisa.autpapo_teste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Tela_Politica_Priv extends AppCompatActivity {
 
+    ImageView btnVoltar;
+    String telaRecebida;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_politica_priv);
+
+        btnVoltar = findViewById(R.id.btnVoltarP);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getIntent().hasExtra("login")) {
+                    // Valor recebido da Tela_Login
+                    Intent abrirLogin = new Intent(Tela_Politica_Priv.this, Tela_Login.class);
+                    startActivity(abrirLogin);
+
+                } else if (getIntent().hasExtra("conta")) {
+                    Intent abrirLogin = new Intent(Tela_Politica_Priv.this, Tela_CriarConta.class);
+                    startActivity(abrirLogin);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
